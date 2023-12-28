1201757c03fe8a85cb422897a9dd64bfcfddfd23
David Wronek <davidwronek@gmail.com>
2023-12-28 16:22:11 +0100

    Update board files

--- board/samsung/t1/t1.c
+++ board/samsung/t1/t1.c
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0+
-/*
- * This empty file prevents make linking error.
- * No custom logic for t1 so far.
- */
 
 void nooop(void) {}
+void set_muxconf_regs(void) {}
+struct omap_sysinfo {
+	char *board_string;
+};
+const struct omap_sysinfo sysinfo = {
+	"Board: Samsung Galaxy S II (GT-I9100G)\n"
+};
+int board_init(void) { return 0; }
+int misc_init_r(void) { return 0; }
