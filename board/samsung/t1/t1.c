84b43c2d1ed8f96967ef00bbe8a1b878a894c2f7
David Wronek <davidwronek@gmail.com>
2023-12-28 13:29:59 +0100

    Update config

---
+++ board/samsung/t1/t1.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * This empty file prevents make linking error.
+ * No custom logic for t1 so far.
+ */
+
+void nooop(void) {}
---
+++ include/configs/omap4_samsung_t1.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Configuration settings for the Samsung T1 board.
+ * See ti_omap4_common.h for OMAP4 common part
+ */
+
+#ifndef __CONFIG_SAMSUNG_T1_H
+#define __CONFIG_SAMSUNG_T1_H
+
+#include <configs/ti_omap4_common.h>
+
+#endif /* __CONFIG_SAMSUNG_T1_H */
